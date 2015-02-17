fe31da9b7cbaf59c57590c9d042baacc7a5e76c4
Sara Falkoff <sfalkoff11@gmail.com>
2015-02-17 15:39:27 -0800

    User can generate simle fractal tree with random branch point angles on mouse click.

--- static/draw_to_canvas.js
+++ static/draw_to_canvas.js
@@ -1,31 +1,65 @@
-function draw() {
+function draw(){
   var canvas = document.getElementById('canvas');
-  if (canvas.getContext) {
-    var ctx = canvas.getContext('2d');
-  } else {
-  	return "Oh no! Your browser doesn't support HTML5 Canvas."
+  if (canvas.getContext){
+    var context = canvas.getContext('2d');
+    drawFractalTree(context); 
   }
-    ctx.beginPath();
-    ctx.moveTo(250,300);
-    ctx.lineTo(250,200);
-    ctx.lineTo(200,125);
-    ctx.moveTo(250,200);
-    ctx.lineTo(300,125);
-    ctx.stroke();
- }
+  else{
+    alert("HTML5 Canvas isn't supported by your browser!");
+  }
+}
 
- $("#canvas").on("click", draw);
+function drawFractalTree(context){
 
-// Recursive Python Fractal Tree:
-// def tree(branchLen,t):
-//     if branchLen > 5:
-//         t.forward(branchLen)
-//         t.right(20)
-//         tree(branchLen-15,t)
-//         t.left(40)
-//         tree(branchLen-15,t)
-//         t.right(20)
-//         t.backward(branchLen)
+	drawTree(context,250,500,-90,8);
+}
+
+function drawTree(context, x1, y1, angle, depth){
+
+	var BRANCH_LENGTH = random(0, 16);
+
+	if (depth != 0){
+		var x2 = x1 + (cos(angle) * depth * BRANCH_LENGTH);
+		var y2 = y1 + (sin(angle) * depth * BRANCH_LENGTH);
+		
+		drawLine(context, x1, y1, x2, y2, depth);
+		drawTree(context, x2, y2, angle - random(15,20), depth - 1);
+		drawTree(context, x2, y2, angle + random(15,20), depth - 1);
+	}
+}
+
+function drawLine(context, x1, y1, x2, y2, thickness){
+	context.fillStyle   = '#000';
+	context.strokeStyle = 'rgb(120, 80, 54)'; //Brown		
+
+	context.lineWidth = thickness;
+	context.beginPath();
+
+	context.moveTo(x1,y1);
+	context.lineTo(x2, y2);
+
+	context.closePath();
+	context.stroke();
+}
 
 
-// }
+function cos (angle) {
+	return Math.cos(deg_to_rad(angle));
+}
+
+function sin (angle) {
+	return Math.sin(deg_to_rad(angle));
+}
+
+function deg_to_rad(angle){
+	return angle*(Math.PI/180.0);
+}
+
+function random(min, max){
+	return min + Math.floor(Math.random()*(max+1-min));
+}
+
+$("#canvas").on("click", draw);
+
+
+
