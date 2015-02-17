cd2edfc95ffd79e70c1d62d3770d9564de582a6a
Sara Falkoff <sfalkoff11@gmail.com>
2015-02-16 16:54:46 -0800

    Removed styling and javascript from home.html and added to style.css and draw_to_canvas.js respectively.

--- static/draw_to_canvas.js
+++ static/draw_to_canvas.js
@@ -3,5 +3,12 @@ function draw(){
  
     if (canvas.getContext){
     	var ctx = canvas.getContext('2d');
+
+    	//FIXME: This is supposed to create two squares. 
+    	ctx.fillStyle = "rgb(200,0,0)";
+        ctx.fillRect (10, 10, 55, 50);
+
+        ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+        ctx.fillRect (30, 30, 55, 50);
     }
 }
