f7ac938f297849e6b9b167be8ba207284cc2825d
Sara Falkoff <sfalkoff11@gmail.com>
2015-02-18 17:13:47 -0800

    Added animation to control execution of drawLine function over a period of time (so the tree looks like it's growing). Added save_img function so user can save image locally as png file.

--- static/draw_to_canvas.js
+++ static/draw_to_canvas.js
@@ -2,25 +2,27 @@ function draw(){
 	var canvas = document.getElementById('canvas');
 	if (canvas.getContext){
 		var ctx = canvas.getContext('2d');
-		ctx.clearRect(0,0,500,500); // clear canvas
-		//window.requestAnimationFrame(draw);
-		drawTree(ctx,250,500,-90,8); 
+		ctx.clearRect(0, 0, 500, 500); // clear canvas
+		drawTree(ctx, 250, 500, -90, 8, 1); // initiate chain of recursive calls
 	}else{
 		alert("HTML5 Canvas isn't supported by your browser!");
 	}
 }
 
-function drawTree(context, x1, y1, angle, depth){
+function drawTree(context, x1, y1, angle, depth, delay){
 
 	var BRANCH_LENGTH = random(0, 16);
 
 	if (depth != 0){
 		var x2 = x1 + (cos(angle) * depth * BRANCH_LENGTH);
 		var y2 = y1 + (sin(angle) * depth * BRANCH_LENGTH);
+	
+		window.setTimeout(function(){
+			drawLine(context, x1, y1, x2, y2, depth);
+		}, 100 * delay);
 
-		drawLine(context, x1, y1, x2, y2, depth);
-		drawTree(context, x2, y2, angle - random(15,20), depth - 1);
-		drawTree(context, x2, y2, angle + random(15,20), depth - 1);
+		drawTree(context, x2, y2, angle - random(15, 20), depth - 1, delay * 1.2);
+		drawTree(context, x2, y2, angle + random(15, 20), depth - 1, delay * 1.2);
 	}
 }
 
@@ -31,7 +33,7 @@ function drawLine(context, x1, y1, x2, y2, thickness){
 	context.lineWidth = thickness;
 	context.beginPath();
 
-	context.moveTo(x1,y1);
+	context.moveTo(x1, y1);
 	context.lineTo(x2, y2);
 
 	context.closePath();
@@ -56,10 +58,15 @@ function random(min, max){
 	return min + Math.floor(Math.random()*(max+1-min));
 }
 
-
 // draw tree on mouse click
 $("#canvas").on("click", draw);
 
-
+// save canvas image as data url (png format by default)
+function save_img() {
+	var canvas = document.getElementById('canvas');
+	var dataURL = canvas.toDataURL();
+	// set canvasImg image src to dataURL so it can be saved as an image
+	document.getElementById('canvasImg').src = dataURL;
+}
 
 
