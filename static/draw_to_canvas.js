7228d678aa1a58a1ec9abcf916a8ac2230131c06
Sara Falkoff <sfalkoff11@gmail.com>
2015-02-27 08:55:30 -0800

    Branch-length slider now gives user ability to control fixed length for tree branches.

--- static/draw_to_canvas.js
+++ static/draw_to_canvas.js
@@ -1,4 +1,8 @@
-// color picker slider
+// global variables
+var branchLength;
+var hex;
+
+// stuff for color picker slider
 function hexFromRGB(r, g, b) {
 	var rgb_hex = [
 	  r.toString( 16 ),
@@ -13,7 +17,7 @@ function hexFromRGB(r, g, b) {
 	
 	// returns hex values for red, green, and blue sliders
 	return rgb_hex.join( "" ).toUpperCase();
-	}
+}
 
 function refreshSwatch() {
 	var red = $( "#red" ).slider( "value" );
@@ -23,16 +27,19 @@ function refreshSwatch() {
 	// creates one hex code with the combined rgb color values
 	hex = hexFromRGB( red, green, blue );
 
+	// sets swatch backbround to user selected color 
 	$( "#swatch" ).css( "background-color", "#" + hex );
-	}
-	$(function() {
-		$( "#red, #green, #blue" ).slider({
-		  orientation: "horizontal",
-		  range: "min",
-		  max: 255,
-		  value: 127,
-		  slide: refreshSwatch,
-		  change: refreshSwatch
+}
+
+//document ready event prevents jQuery from loading before document has loaded
+$(function() {
+	$( "#red, #green, #blue" ).slider({
+	  orientation: "horizontal",
+	  range: "min",
+	  max: 255,
+	  value: 127,
+	  slide: refreshSwatch,
+	  change: refreshSwatch
 	});
 	$( "#red" ).slider( "value", 255 );
 	$( "#green" ).slider( "value", 140 );
@@ -41,12 +48,11 @@ function refreshSwatch() {
 // end color picker slider
 
 
-//branch length slider
+// branch length slider
 $(function() {
-	$("#slider").slider({max: 16, min: 0, value: 8})
+	$("#slider").slider({max: 16, min: 0, value: 8});
 	$("#slider").on("slide", function(event, ui) {
-		value = ui.value;
-		//console.log(value);
+		branchLength = ui.value;
 	});
 });
 // end branch length slider
@@ -58,34 +64,32 @@ function draw(){
 	if (canvas.getContext){
 		var ctx = canvas.getContext('2d');
 		ctx.clearRect(0, 0, 600, 600); // clear canvas
-		drawTree(ctx, 300, 600, -90, 9, 1); // initiate chain of recursive calls
+		drawTree(ctx, 300, 600, -90, branchLength, 9, 1); // initiate chain of recursive calls
 	}else{
 		alert("HTML5 Canvas isn't supported by your browser!");
 	}
 }
 
-//function drawTree(context, x1, y1, angle, branchLength, depth, delay){
-function drawTree(context, x1, y1, angle, depth, delay){
-
-	var BRANCH_LENGTH = random(0, 16);
-	//console.log(value);
+function drawTree(context, x1, y1, angle, branchLength, depth, delay){
+//function drawTree(context, x1, y1, angle, depth, delay){
+	//var BRANCH_LENGTH = random(0, 16);
 	
 	if (depth != 0){
-		var x2 = x1 + (cos(angle) * depth * BRANCH_LENGTH);
-		var y2 = y1 + (sin(angle) * depth * BRANCH_LENGTH);
-		// var x2 = x1 + (cos(angle) * depth * value);
-		// var y2 = y1 + (sin(angle) * depth * value);
+		// var x2 = x1 + (cos(angle) * depth * BRANCH_LENGTH);
+		// var y2 = y1 + (sin(angle) * depth * BRANCH_LENGTH);
+		var x2 = x1 + (cos(angle) * depth * branchLength);
+		var y2 = y1 + (sin(angle) * depth * branchLength);
 	
 		window.setTimeout(function(){
 			drawLine(context, x1, y1, x2, y2, depth, hex);
 		}, 100 * delay);
 
-		// drawTree(context, x2, y2, angle - value, depth - 1, delay * 1.2);
-		// drawTree(context, x2, y2, angle + value, depth - 1, delay * 1.2);
+		drawTree(context, x2, y2, angle - branchLength, branchLength, depth - 1, delay * 1.2);
+		drawTree(context, x2, y2, angle + branchLength, branchLength, depth - 1, delay * 1.2);
 		
 
-		drawTree(context, x2, y2, angle - random(15, 20), depth - 1, delay * 1.2);
-		drawTree(context, x2, y2, angle + random(15, 20), depth - 1, delay * 1.2);
+		// drawTree(context, x2, y2, angle - random(15, 20), depth - 1, delay * 1.2);
+		// drawTree(context, x2, y2, angle + random(15, 20), depth - 1, delay * 1.2);
 	}
 }
 
