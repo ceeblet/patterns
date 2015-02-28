c1c62ebf87bd0ecdaeef55320e53c396fe5c3bba
Sara Falkoff <sfalkoff11@gmail.com>
2015-02-27 17:45:53 -0800

    User can save the image they created, now saved to S3.

--- controller.py
+++ controller.py
@@ -19,9 +19,11 @@ app.secret_key = 'fU0Og5yop7EddZQOGUE$FMENpdw1'
 def home():
     return render_template('home.html')
 
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
 
 @app.route('/gallery')
 def gallery():
@@ -29,6 +31,34 @@ def gallery():
 	images = Image.query.limit(15).all()
 	return render_template('gallery.html', images = images)
 
+
+@app.route('/save', methods=['POST'])
+def save_img():
+	boto.set_stream_logger('boto')
+
+	s3 = boto.connect_s3()
+
+	# access secret keys
+	AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+	AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+
+	# Q: should I not be doing this every time?
+	# create a new bucket
+	bucket = s3.create_bucket('fractal-art')
+
+	_, b64data = str(request.form.get('data')).split(',')
+
+	decoded_img = decode_img(b64data)
+
+	filename = str(uuid.uuid4()) + '.png'
+
+	# create a new key/value pair
+	key = bucket.new_key(filename)
+	key.set_contents_from_string(decoded_img)
+
+	return 'successful upload'
+
+# saves images to file system on disk
 # @app.route('/save', methods=['POST'])
 # def save_img():
 # 	# get dataURL from draw_to_canvas.js and convert from unicode to string
@@ -59,74 +89,6 @@ def gallery():
 # 	return "Image saved!"
 
 
-# WIP: Make this route to save image url to s3
-@app.route('/save', methods=['POST'])
-def save_img():
-	boto.set_stream_logger('boto')
-
-	s3 = boto.connect_s3()
-
-	# Access secret keys
-	AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-	AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-
-	# Q: should I not be doing this every time?
-	# Create a new bucket
-	bucket = s3.create_bucket('fractal-art')
-
-	fp = '/Users/sarafalkoff/fractal-art/static/img_uploads/9ba91c79-312a-4f58-b040-2f92b34a1615.png'
-
-	file_1 = open(fp)
-	img_1 = file_1.read()
-
-
-	# Create a new key/value pair
-	key = bucket.new_key("Test_Img.png")
-	key.set_contents_from_string(img_1)
-	# key.set_contents_from_file
-
-	# Retrieve the contents of 'mykey'
-	print key.get_contents_as_string()
-	return 'successful upload'
-
-
-# import boto
-# import cStringIO
-# import urllib
-# import Image
-
-# #Retrieve our source image from a URL
-# fp = urllib.urlopen('http://example.com/test.png')
-
-# #Load the URL data into an image
-# img = cStringIO.StringIO(fp.read())
-# im = Image.open(img)
-
-# #Resize the image
-# im2 = im.resize((500, 100), Image.NEAREST)  
-
-# #NOTE, we're saving the image into a cStringIO object to avoid writing to disk
-# out_im2 = cStringIO.StringIO()
-# #You MUST specify the file type because there is no file name to discern it from
-# im2.save(out_im2, 'PNG')
-
-# #Now we connect to our s3 bucket and upload from memory
-# #credentials stored in environment AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
-# conn = boto.connect_s3()
-
-# #Connect to bucket and create key
-# b = conn.get_bucket('example')
-# k = b.new_key('example.png')
-
-# #Note we're setting contents from the in-memory string provided by cStringIO
-# k.set_contents_from_string(out_im2.getvalue())
-
-
-
-
-
-
- 
 
 # @app.route('/save', methods=['POST'])
 # def tweet_img():
