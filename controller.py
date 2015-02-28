2fc264d02803585831d52185b117f26adca1740b
Sara Falkoff <sfalkoff11@gmail.com>
2015-02-28 09:23:54 -0800

    Added clarifying comments to save_img().

--- controller.py
+++ controller.py
@@ -34,38 +34,49 @@ def gallery():
 
 @app.route('/save', methods=['POST'])
 def save_img():
+	'''Saves user created image to database and s3'''
+	
 	boto.set_stream_logger('boto')
 
+	# create a connection to s3Connection object
 	s3 = boto.connect_s3()
 
 	# access secret keys
 	AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 	AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
-	# Q: should I not be doing this every time?
-	# create a new bucket
+	# create new bucket if named bucket does not exist and return exisiting bucket if it already exists
 	bucket = s3.create_bucket('fractal-art')
 
+	# get dataURL from draw_to_canvas.js and convert from unicode to string
+	# split dataURL and ignore leading 'data:image/png;base64'
 	_, b64data = str(request.form.get('data')).split(',')
 
+	# decode base64 data string ensuring the length is a multiple of 4 bytes
 	decoded_img = decode_img(b64data)
 
 	path = 'https://s3.amazonaws.com/fractal-art/'
 
 	# TODO: UUID collision avoidence 
+	# create unique file name for user image
 	filename = str(uuid.uuid4()) + '.png'
 
+	# create full filepath (a url) for user image
 	fullpath = os.path.join(path, filename)
 
-	# create a new key/value pair
+	# create a new s3 key/value pair
 	key = bucket.new_key(filename)
 	key.set_contents_from_string(decoded_img)
 
+	# instantiate instance of Image class
 	img = Image(fullpath, filename)
 	
+	# save image to database
 	img.save()
 
 	return 'successful upload'
+
+
 
 
 # saves images to file system on disk
